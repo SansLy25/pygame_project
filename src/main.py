eb0fdda63034f536b78e223a74dd05ccf987ac8f
sansly <robertstepa017@gmail.com>
2025-02-05 00:54:22 +0300

    fix(conflict): conflict error

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import pygame
-from engine.objects import Player, GameObject
+from engine.objects import Player, GameObject, Enemy
 from engine.app import App
 from engine.animation import Animation
 from engine.vectors import Vector, Acceleration, Speed
@@ -14,6 +14,12 @@ def hover_check(event):
         app.exit_button.check_hover(event.pos)
         app.settings_button.check_hover(event.pos)
         app.back1_button.check_hover(event.pos)
+        app.upgrade_manager.crit_button.check_hover(event.pos)
+        app.upgrade_manager.hp_button.check_hover(event.pos)
+        app.upgrade_manager.crit_chance_button.check_hover(event.pos)
+        app.upgrade_manager.damage_button.check_hover(event.pos)
+        app.upgrade_manager.attack_speed_button.check_hover(event.pos)
+        app.upgrade_manager.cancel_button.check_hover(event.pos)
 
 
 if __name__ == "__main__":
@@ -29,15 +35,19 @@ if __name__ == "__main__":
     pygame.display.set_caption("GameObject Example")
 
     game_object_animation = Animation(
-        [f'../assets/adventurer-0{i}.png' for i in
-         range(1, 6)], 100)
+        [f'../assets/adventurer-0{i + 1}.png' for i in
+         range(5)], 100)
 
-    game_object = Player(0, -100, 100, 120,
+    game_object = Player(100, 100, 100, 100,
                          sprite_path="../assets/adventurer-00.png",
                          a0=Acceleration(1,
                                          Vector.unit_from_angle(
                                              90)),
                          animation=game_object_animation)
+
+    enemy = Enemy(500, 100, 100, 100, sprite_path="../assets/adventurer-00.png",
+                  a0=Acceleration(1, Vector.unit_from_angle(90)))
+    enemy.set_target(game_object)
 
     surface = GameObject(0, 200, 1000, 1000)
     running = True
@@ -46,28 +56,30 @@ if __name__ == "__main__":
     game_started = False
     is_paused = False
     is_settings = False
+    tick_count = 0
 
     while running:
         keys = pygame.key.get_pressed()
         events = pygame.event.get()
+        mouse = pygame.mouse.get_pressed()
         for event in events:
             if event.type == pygame.QUIT:
                 running = False
-            if not game_started and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and app.play_button.is_hovered:
+            if not game_started and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and app.play_button.is_hovered: # меню
                 game_started = True
                 is_paused = False
                 pygame.mixer.music.stop()
                 app.is_menu_music = False
                 pygame.mixer.music.load('../assets/stage1.mp3')
                 pygame.mixer.music.play(-1)
-            if not is_settings:
-                if is_paused and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and app.resume_button.is_hovered:
+            if not is_settings and is_paused: # пауза
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and app.resume_button.is_hovered:
                     is_paused = not is_paused
-                if is_paused and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and app.exit_button.is_hovered:
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and app.exit_button.is_hovered:
                     game_started = False
-                if is_paused and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and app.settings_button.is_hovered:
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and app.settings_button.is_hovered:
                     is_settings = True
-            else:
+            else: # настройки
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and app.back1_button.is_hovered:
                     is_settings = False
             if app.is_lvlup: # меню улучшений
@@ -89,13 +101,13 @@ if __name__ == "__main__":
                     app.is_lvlup = False
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and app.upgrade_manager.cancel_button.is_hovered:
                     app.is_lvlup = False
-										
             hover_check(event)
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 is_paused = not is_paused
                 is_settings = False
 
         if game_started:
+            enemy.can_be_attacked()
             screen.fill((0, 0, 0))
             if not is_paused:
                 if not app.is_lvlup:
@@ -126,18 +138,18 @@ if __name__ == "__main__":
                                                                           180))
                         game_object.target_orientation = 'left'
 
-                    
+                    all_game_objects = GameObject.all_game_objects
+
+                    for object in all_game_objects:
+                        object.update(screen, [obj for obj in all_game_objects if obj != object])
+
                     app.expbar.update(game_object.current_exp, game_object.max_exp)
                     app.expbar.draw()
                     app.hpbar.update(game_object.current_hp, game_object.max_hp)
                     app.hpbar.draw()
-                    all_game_objects = GameObject.all_game_objects
-               			for object in all_game_objects:
-                    		object.update(screen, [obj for obj in all_game_objects if obj != object])
-												
+
                 elif app.is_lvlup:
                     app.upgrade_manager.draw()
-												
             elif is_paused:
                 if not is_settings:
                     app.pause()
@@ -149,6 +161,6 @@ if __name__ == "__main__":
 
         pygame.display.flip()
         clock.tick(60)
+        tick_count += 1
 
     pygame.quit()
-    
